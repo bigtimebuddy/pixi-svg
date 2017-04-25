d185f97f14fccea44dd80e0da788182f9b4cc011
Matt Karl <matt.karl@jibo.com>
2017-04-25 08:28:08 -0400

    Created SVG object

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
 # pixi-svg
 
-SVG to Graphics Converter for pixi.js
+SVG to Graphics DisplayObject for PIXI.
+
+## Install
+
+```bash
+npm install pixi-svg --save
+```
+
+## Usage
+
+For an inline SVG element:
+
+```html
+<svg id="svg1" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+  <circle r="50" cx="50" cy="50" fill="#F00" />
+</svg>
+```
+
+Create a new `PIXI.SVG` object, provide the `<svg>` element.
+
+```js
+const svg = new PIXI.SVG(document.getElementById("svg1"));
+const app = new PIXI.Application();
+app.stage.addChild(svg);
+```
+
+## License
+
+MIT License.
---
+++ src/SVG.js
@@ -0,0 +1,318 @@
+import dPathParse from 'd-path-parser';
+
+// <div> element to measure string colors like "black"
+// and convert to hex colors
+const measureColor = document.createElement('div');
+
+/**
+ * Scalable Graphics drawn from SVG image document.
+ * @class SVG
+ * @extends PIXI.Graphics
+ * @memberof PIXI
+ * @param {SVGSVGElement} svg - SVG Element `<svg>`
+ */
+export default class SVG extends PIXI.Graphics {
+    /**
+     * Constructor
+     */
+    constructor (svg) {
+        super();
+        this.fill(svg);
+        this.svgChildren(svg.children);
+    }
+
+    /**
+     * Create a PIXI Graphic from SVG element
+     * @private
+     * @method PIXI.SVG#svgChildren
+     * @param {Array<*>} children - Collection of SVG nodes
+     * @param {Boolean} [inherit=false] Whether to inherit fill settings.
+     */
+    svgChildren (children, inherit = false) {
+        for (let i = 0; i < children.length; i++) {
+            const child = children[i];
+            this.fill(child, inherit);
+            switch (child.nodeName.toLowerCase()) {
+                case 'path': {
+                    this.svgPath(child);
+                    break;
+                }
+                case 'circle':
+                case 'ellipse': {
+                    this.svgCircle(child);
+                    break;
+                }
+                case 'rect': {
+                    this.svgRect(child);
+                    break;
+                }
+                case 'g': {
+                    break;
+                }
+                default: {
+                    // @if DEBUG
+                    console.info('[SVGUtils] <%s> elements unsupported', child.nodeName);
+                    // @endif
+                    break;
+                }
+            }
+            this.svgChildren(child.children, true);
+        }
+    }
+
+    /**
+     * Convert the Hexidecimal string (e.g., "#fff") to uint
+     * @private
+     * @method PIXI.SVG#hexToUint
+     */
+    hexToUint (hex) {
+        if (hex[0] === '#') {
+                        // Remove the hash
+            hex = hex.substr(1);
+
+                        // Convert shortcolors fc9 to ffcc99
+            if (hex.length === 3) {
+                hex = hex.replace(/([a-f0-9])/ig, '$1$1');
+            }
+            return parseInt(hex, 16);
+        } else {
+            measureColor.style.color = hex;
+            const rgb = window.getComputedStyle(document.body.appendChild(measureColor)).color
+                .match(/\d+/g)
+                .map(function (a) {
+                    return parseInt(a, 10);
+                });
+            document.body.removeChild(measureColor);
+            return (rgb[0] << 16) + (rgb[1] << 8) + rgb[2];
+        }
+    }
+
+    /**
+     * Render a <ellipse> element or <circle> element
+     * @private
+     * @method PIXI.SVG#internalEllipse
+     * @param {SVGCircleElement} node
+     */
+    svgCircle (node) {
+
+        let heightProp = 'r';
+        let widthProp = 'r';
+        const isEllipse = node.nodeName === 'elipse';
+        if (isEllipse) {
+            heightProp += 'x';
+            widthProp += 'y';
+        }
+        const width = parseFloat(node.getAttribute(widthProp));
+        const height = parseFloat(node.getAttribute(heightProp));
+        const cx = node.getAttribute('cx');
+        const cy = node.getAttribute('cy');
+        let x = 0;
+        let y = 0;
+        if (cx !== null) {
+            x = parseFloat(cx);
+        }
+        if (cy !== null) {
+            y = parseFloat(cy);
+        }
+        if (!isEllipse) {
+            this.drawCircle(x, y, width);
+        }
+        else {
+            this.drawEllipse(x, y, width, height);
+        }
+    }
+
+    /**
+     * Render a <rect> element
+     * @private
+     * @method PIXI.SVG#svgRect
+     * @param {SVGRectElement} node
+     */
+    svgRect (node) {
+        const x = parseFloat(node.getAttribute('x'));
+        const y = parseFloat(node.getAttribute('y'));
+        const width = parseFloat(node.getAttribute('width'));
+        const height = parseFloat(node.getAttribute('height'));
+        const rx = parseFloat(node.getAttribute('rx'));
+        if (rx) {
+            this.drawRoundedRect(
+                x,
+                y,
+                width,
+                height,
+                rx
+            );
+        } else {
+            this.drawRect(
+                x,
+                y,
+                width,
+                height
+            );
+        }
+    }
+
+    /**
+     * Set the fill and stroke style.
+     * @private
+     * @method PIXI.SVG#fill
+     * @param {SVGElement} node
+     * @param {Boolean} inherit
+     */
+    fill (node, inherit) {
+        const fill = node.getAttribute('fill');
+        const opacity = node.getAttribute('opacity');
+        const stroke = node.getAttribute('stroke');
+        const strokeWidth = node.getAttribute('stroke-width');
+        const lineWidth = strokeWidth !== null ? parseFloat(strokeWidth) : 0;
+        const lineColor = stroke !== null ? this.hexToUint(stroke) : this.lineColor;
+        if (fill) {
+            if (fill === 'none') {
+                this.beginFill(0, 0);
+            } else {
+                this.beginFill(
+                    this.hexToUint(fill),
+                    opacity !== null ? parseFloat(opacity) : 1
+                );
+            }
+        } else if (!inherit) {
+            this.beginFill(0);
+        }
+        this.lineStyle(
+            lineWidth,
+            lineColor
+        );
+
+        // @if DEBUG
+        if (node.getAttribute('stroke-linejoin')) {
+            console.info('[SVGUtils] "stroke-linejoin" attribute is not supported');
+        }
+        if (node.getAttribute('stroke-linecap')) {
+            console.info('[SVGUtils] "stroke-linecap" attribute is not supported');
+        }
+        if (node.getAttribute('fill-rule')) {
+            console.info('[SVGUtils] "fill-rule" attribute is not supported');
+        }
+        // @endif
+    }
+
+    /**
+     * Render a <path> d element
+     * @method PIXI.SVG#svgPath
+     * @param {SVGPathElement} node
+     */
+    svgPath (node) {
+        const d = node.getAttribute('d');
+        let x, y;
+        const commands = dPathParse(d);
+        for (var i = 0; i < commands.length; i++) {
+            const command = commands[i];
+            switch (command.code) {
+                case 'm': {
+                    this.moveTo(
+                        x += command.end.x,
+                        y += command.end.y
+                    );
+                    break;
+                }
+                case 'M': {
+                    this.moveTo(
+                        x = command.end.x,
+                        y = command.end.y
+                    );
+                    break;
+                }
+                case 'H': {
+                    this.lineTo(x = command.value, y);
+                    break;
+                }
+                case 'h': {
+                    this.lineTo(x += command.value, y);
+                    break;
+                }
+                case 'V': {
+                    this.lineTo(x, y = command.value);
+                    break;
+                }
+                case 'v': {
+                    this.lineTo(x, y += command.value);
+                    break;
+                }
+                case 'Z': {
+                    this.closePath();
+                    break;
+                }
+                case 'L': {
+                    this.lineTo(
+                        x = command.end.x,
+                        y = command.end.y
+                    );
+                    break;
+                }
+                case 'l': {
+                    this.lineTo(
+                        x += command.end.x,
+                        y += command.end.y
+                    );
+                    break;
+                }
+                case 'C': {
+                    const currX = x;
+                    const currY = y;
+                    this.bezierCurveTo(
+                        currX + command.cp1.x,
+                        currY + command.cp1.y,
+                        currX + command.cp2.x,
+                        currY + command.cp2.y,
+                        x = command.end.x,
+                        y = command.end.y
+                    );
+                    break;
+                }
+                case 'c': {
+                    const currX = x;
+                    const currY = y;
+                    this.bezierCurveTo(
+                        currX + command.cp1.x,
+                        currY + command.cp1.y,
+                        currX + command.cp2.x,
+                        currY + command.cp2.y,
+                        x += command.end.x,
+                        y += command.end.y
+                    );
+                    break;
+                }
+                case 's':
+                case 'q': {
+                    const currX = x;
+                    const currY = y;
+                    this.quadraticCurveTo(
+                        currX + command.cp.x,
+                        currY + command.cp.y,
+                        x += command.end.x,
+                        y += command.end.y
+                    );
+                    break;
+                }
+                case 'S':
+                case 'Q': {
+                    const currX = x;
+                    const currY = y;
+                    this.quadraticCurveTo(
+                        currX + command.cp.x,
+                        currY + command.cp.y,
+                        x = command.end.x,
+                        y = command.end.y
+                    );
+                    break;
+                }
+                default: {
+                    // @if DEBUG
+                    console.info('[SVGUtils] Draw command not supported:', command.code, command);
+                    // @endif
+                    break;
+                }
+            }
+        }
+    }
+}
